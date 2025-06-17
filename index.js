f27d1ab6f363dcda34e2862d57e358c4fcc33cbb
Yakovyakov <yyak0423@gmail.com>
2025-06-17 15:33:59 -0400

    Exercise 13.4

--- index.js
+++ index.js
@@ -1,16 +1,51 @@
 require('dotenv').config()
-const { Sequelize } = require('sequelize')
+const { PORT } = require('./util/config')
+const { connectToDatabase } = require('./util/db')
 
-const sequelize = new Sequelize(process.env.DATABASE_URL)
+const Blog = require('./models/blog.js')
 
-const main = async () => {
-  try {
-    await sequelize.authenticate()
-    console.log('Connection has been established successfully.')
-    sequelize.close()
-  } catch (error) {
-    console.error('Unable to connect to the database:', error)
+
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+app.get('/api/blogs', async (req, res) => {
+  const blogs = await Blog.findAll()
+  res.json(blogs)
+})
+
+app.get('/api/blogs/:id', async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id)
+  if (blog) {
+    res.json(blog)
+  } else {
+    res.status(404).end()
   }
+})
+
+app.post('/api/blogs', async (req, res) => {
+  try {
+    console.log(req.body)
+    const blog = await Blog.create(req.body)
+    res.json(blog)
+  } catch(error) {
+    return res.status(400).json({ error })
+  }
+})
+
+app.delete('/api/blogs/:id', async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id)
+  if (blog) {
+    await blog.destroy()
+  }
+  res.status(204).end()
+})
+
+const start = async () => {
+  await connectToDatabase()
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
 }
 
-main()
+start()
