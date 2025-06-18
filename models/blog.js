bdd23d4c65c22d867ee680878bfacab450ece991
Yakovyakov <yyak0423@gmail.com>
2025-06-18 15:32:39 -0400

    error validations in model

--- models/blog.js
+++ models/blog.js
@@ -17,15 +17,43 @@ Blog.init({
   url: {
     type: DataTypes.TEXT,
     allowNull: false,
+    validate: {
+      notNull: {
+        msg: 'URL is required'
+      },
+      notEmpty: {
+        msg: 'URL cannot be empty'
+      },
+      isUrl: {
+        msg: 'Must be a valid URL'
+      }
+    }
   },
   title: {
     type: DataTypes.TEXT,
     allowNull: false,
+    validate: {
+      notNull: {
+        msg: 'Title is required'
+      },
+      notEmpty: {
+        msg: 'Title cannot be empty'
+      }
+    }  
   },
   likes: {
     type: DataTypes.INTEGER,
     allowNull: false,
     defaultValue: 0,
+    validate: {
+      isInt: {
+        msg: 'Likes must be an integer'
+      },
+      min: {
+        args: [0],
+        msg: 'Likes cannot be negative'
+      }
+    }
   }
 }, {
   sequelize,
