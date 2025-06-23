7e944a0cacd3d50b9cfd1fec12413ebcf2d94c64
Yakovyakov <yyak0423@gmail.com>
2025-06-23 12:14:20 -0400

    13.16

---
+++ controllers/authors.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+
+const { sequelize } = require('../util/db');
+const { Blog } = require('../models');
+
+router.get('/', async (req, res) => {
+  try {
+    const authorsStats = await Blog.findAll({
+      attributes: [
+        'author',
+        [sequelize.fn('COUNT', sequelize.col('id')), 'articles'],
+        [sequelize.fn('SUM', sequelize.col('likes')), 'likes']
+      ],
+      group: ['author'],
+      order: [[sequelize.fn('SUM', sequelize.col('likes')), 'DESC']],
+      raw: true
+    });
+
+      res.json(authorsStats);
+    } catch (error) {
+      console.error('Error fetching author stats:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const { connectToDatabase } = require('./util/db')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
+const authorsRouter = require('./controllers/authors')
 
 const { unknownEndpoint, errorHandler } = require('./util/middleware')
 
@@ -18,6 +19,7 @@ app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+app.use('/api/authors', authorsRouter)
 
 
 app.use(unknownEndpoint)
