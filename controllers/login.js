c1ac7d74dee902ca550e36809c18bb9f76d0a22f
Yakovyakov <yyak0423@gmail.com>
2025-07-03 17:05:38 -0400

    refactor login.js: add work with sessions

--- controllers/login.js
+++ controllers/login.js
@@ -2,35 +2,49 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const router = require('express').Router()
 
-const User = require('../models/user')
+const { User, Session } = require('../models')
 
-router.post('/', async (request, response) => {
+router.post('/', async (request, response, next) => {
   const { username, password } = request.body
 
   if (password === undefined || username === undefined) {
     return response.status(400).json({ error: 'username or password is missing' })
   }
 
-  const user = await User.findOne({
-    where: {
-      username,
-    },
-  })
+  try {
+    const user = await User.findOne({
+      where: {
+        username,
+      },
+    })
 
-  const passwordCorrect = user === null ? false : await bcrypt.compare(password, user.passwordHash)
+    const passwordCorrect =
+      user === null ? false : await bcrypt.compare(password, user.passwordHash)
 
-  if (!(user && passwordCorrect)) {
-    return response.status(401).json({ error: 'invalid username or password' })
+    if (!(user && passwordCorrect)) {
+      return response.status(401).json({ error: 'invalid username or password' })
+    }
+
+    if (user.disabled) {
+      await Session.destroy({ where: { userId: user.id } })
+      return response.status(401).json({ error: 'Account disabled' })
+    }
+    const userForToken = {
+      username: user.username,
+      id: user.id,
+    }
+
+    const token = jwt.sign(userForToken, process.env.SECRET, { expiresIn: 60 * 120 })
+
+    await Session.create({
+      token,
+      userId: user.id,
+    })
+
+    response.status(200).send({ token, username: user.username, name: user.name })
+  } catch (err) {
+    next(err)
   }
-
-  const userForToken = {
-    username: user.username,
-    id: user.id,
-  }
-
-  const token = jwt.sign(userForToken, process.env.SECRET, { expiresIn: 60 * 120 })
-
-  response.status(200).send({ token, username: user.username, name: user.name })
 })
 
 module.exports = router
