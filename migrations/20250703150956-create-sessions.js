2ccc4d2f064cf4638be40fb767c32ea0ad3f0cc8
Yakovyakov <yyak0423@gmail.com>
2025-07-03 11:33:50 -0400

    migrations: add disabled column to users table, create sessions table

---
+++ migrations/20250703150956-create-sessions.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const { DataTypes, Sequelize } = require('sequelize');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('sessions', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      token: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        onDelete: 'CASCADE'
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        onUpdate: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('sessions');
+  }
+};
